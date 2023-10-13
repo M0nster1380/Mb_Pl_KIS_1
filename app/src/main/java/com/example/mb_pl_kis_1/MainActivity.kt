ce2c838095b2f66ba0b33712d26ba7b9bb7ba6a4
M0nster_1380 <56561500+M0nster1380@users.noreply.github.com>
2023-10-13 12:45:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mb_pl_kis_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mb_pl_kis_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mb_Pl_KIS_1"
+include(":app")
+ 
